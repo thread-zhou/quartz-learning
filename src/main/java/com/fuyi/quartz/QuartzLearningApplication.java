e3fc7b2a279f6552fa5b53a0db865e7ccb8e5450
zhoujian <thread_j@163.com>
2021-05-28 17:29:09 +0800

    init

---
+++ src/main/java/com/fuyi/quartz/QuartzLearningApplication.java
@@ -0,0 +1,13 @@
+package com.fuyi.quartz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuartzLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuartzLearningApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fuyi/quartz/QuartzLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fuyi.quartz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuartzLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
